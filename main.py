ca5c64eee02c1ae5a00b76d58038bf4cd3e3b725
Tom Dörr <tom.doerr@tum.de>
2021-09-06 06:52:20 +0200

    Add file

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+'''
+This is a very simple script that uses huggingfaces to download a transformer 
+which completes the text the user inputs.
+'''
+
+import argparse
+import os
+import sys
+import logging
+
+from transformers import pipeline
+
+logger = logging.getLogger(__name__)
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-s', '--seed', help='The seed text to complete.', required=True)
+    parser.add_argument('-n', '--number', help='The number of completions to generate.', type=int, default=5)
+    parser.add_argument('-l', '--length', help='The number of tokens to complete to.', type=int, default=20)
+    parser.add_argument('-o', '--output', help='The file to write completions to. If not specified, completions will be written to stdout.')
+    parser.add_argument('-v', '--verbose', help='Print debug information to stderr.', action='store_true')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    logger.info("Downloading pipeline...")
+    pipeline_name = 'fill-mask'
+    # pipeline_kwargs = {'top_k': args.number, 'max_length': args.length}
+    pipeline_kwargs = {'top_k': args.number}
+    pipeline_class = pipeline(pipeline_name, **pipeline_kwargs)
+
+    logger.info("Generating completions...")
+    # completions = pipeline_class(args.seed)
+    completions = pipeline_class('Customer complaint: <mask>')
+
+    if args.output is None:
+        for completion in completions:
+            print(completion)
+    else:
+        with open(args.output, 'w') as f:
+            for completion in completions:
+                f.write(completion + '\n')
+
+    logger.info("Done.")
+
+if __name__ == '__main__':
+    main()
+
